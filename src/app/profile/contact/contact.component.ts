bf7d07535155d52f322a5c20918b7a9c9b614ce1
@Vineeta.Samson <Vineeta.Samson@ust.com>
2023-04-13 01:29:48 +0530

    first commit

---
+++ src/app/profile/contact/contact.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { environment } from '../../../environments/environment';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+ 
+  model: any = {};
+
+  constructor(){}
+
+  ngOnInit() {
+     }
+     detailssubmit(){
+      alert("Contact details Submitted!");
+     }
+
+}
